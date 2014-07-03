c8b4cce5bff04d7fc1957f7845aa2d0e3aec3b7f
Nicolas Melo <nicolas@path.com>
2014-07-02 19:05:50 -0700

    Added LoginViewController xib files and pushing.

---
+++ THC/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  THC
+//
+//  Created by Nicolas Melo on 7/2/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
