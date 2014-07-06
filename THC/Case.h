9b7e950b7152016390870b60f06f484dd57dcfa5
Dave Bernthal <dbernthal@okta.com>
2014-07-05 18:01:45 -0700

    Added Case model file

---
+++ THC/Case.h
@@ -0,0 +1,13 @@
+//
+//  Case.h
+//  THC
+//
+//  Created by David Bernthal on 7/5/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface Case : PFObject
+
+@end
---
+++ THC/Case.m
@@ -0,0 +1,13 @@
+//
+//  Case.m
+//  THC
+//
+//  Created by David Bernthal on 7/5/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import "Case.h"
+
+@implementation Case
+
+@end
