da0617a5e42ba38b53fb3fc47a3c086e6b238806
Hunaid <hunaid@hotmail.com>
2014-07-07 08:53:46 -0700

    Additional form for Other address

---
+++ THC/OtherAddress.h
@@ -0,0 +1,13 @@
+//
+//  OtherAddress.h
+//  THC
+//
+//  Created by Hunaid Hussain on 7/7/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OtherAddress : NSObject
+
+@end
---
+++ THC/OtherAddress.m
@@ -0,0 +1,13 @@
+//
+//  OtherAddress.m
+//  THC
+//
+//  Created by Hunaid Hussain on 7/7/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import "OtherAddress.h"
+
+@implementation OtherAddress
+
+@end
