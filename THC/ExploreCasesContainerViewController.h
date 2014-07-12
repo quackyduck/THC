bb41d945ed73045cc8ff4e6f37107c1613010526
Nicolas Melo <nicolas@path.com>
2014-07-12 13:56:40 -0700

    Files for custom container view controller.

---
+++ THC/ExploreCasesContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExploreCasesContainerViewController.h
+//  THC
+//
+//  Created by Nicolas Melo on 7/12/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExploreCasesContainerViewController : UIViewController
+
+@end
---
+++ THC/ExploreCasesContainerViewController.m
@@ -0,0 +1,38 @@
+//
+//  ExploreCasesContainerViewController.m
+//  THC
+//
+//  Created by Nicolas Melo on 7/12/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import "ExploreCasesContainerViewController.h"
+
+@interface ExploreCasesContainerViewController ()
+
+@end
+
+@implementation ExploreCasesContainerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
