2501f77acdd368dbf5de8129fc9e0b8593450cb2
Nicolas Melo <nicolas@path.com>
2014-07-17 21:59:22 -0700

    Working hacked segmented control with new create report

--- THC/ExploreCasesContainerViewController.m
+++ THC/ExploreCasesContainerViewController.m
@@ -9,22 +9,20 @@
 #import "ExploreCasesContainerViewController.h"
 #import "AggregateMapViewController.h"
 #import "CaseTableViewController.h"
-#import "ExploreTabBarItem.h"
 #import "ViolationSubmissionViewController.h"
 
 @interface ExploreCasesContainerViewController ()
 @property (weak, nonatomic) IBOutlet UIView *containerView;
-@property (weak, nonatomic) IBOutlet UIView *createReportView;
-@property (weak, nonatomic) IBOutlet ExploreTabBarItem *exploreTabBar;
 @property (weak, nonatomic) IBOutlet UIButton *createReportButton;
 @property (strong, nonatomic) NSArray *tabViewControllers;
-@property (weak, nonatomic) IBOutlet UIView *containerBackgroundView;
-@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tapGestureRecognizer;
-- (IBAction)onTapTabBar:(UITapGestureRecognizer *)sender;
+@property (weak, nonatomic) IBOutlet UIView *dividerView;
+@property (weak, nonatomic) IBOutlet UIButton *casesButton;
+@property (weak, nonatomic) IBOutlet UIButton *nearbyButton;
+
 
 - (IBAction)onCreateReport:(id)sender;
-
-
+- (IBAction)onNearbyTab:(id)sender;
+- (IBAction)onCasesTab:(id)sender;
 
 @end
 
@@ -47,11 +45,7 @@
 {
     [super viewDidLoad];
     [self setNeedsStatusBarAppearanceUpdate];
-    
-    AggregateMapViewController *mapsViewController = self.tabViewControllers[0];
-    UINavigationController *nvc = self.tabViewControllers[1];
-    
-    [self show:mapsViewController andRemove:nvc];
+    [self selectedNearbyButton];
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,7 +55,40 @@
 }
 
 -(UIStatusBarStyle) preferredStatusBarStyle {
-    return UIStatusBarStyleLightContent;
+    return UIStatusBarStyleDefault;
+}
+
+- (void)selectedNearbyButton {
+    UIColor *selectedColor = [UIColor colorWithRed:35/255.0f green:41/255.0f blue:52/255.0f alpha:1.0f];
+    UIColor *unselectedColor = [UIColor colorWithRed:149/255.0f green:150/255.0f blue:151/255.0f alpha:1.0f];
+    
+    self.nearbyButton.imageView.image = [UIImage imageNamed:@"ic_nav_map_selected"];
+    self.nearbyButton.titleLabel.textColor = selectedColor;
+    
+    self.casesButton.imageView.image = [UIImage imageNamed:@"ic_nav_cases_normal"];
+    self.casesButton.titleLabel.textColor = unselectedColor;
+    
+    AggregateMapViewController *mapsViewController = self.tabViewControllers[0];
+    UINavigationController *nvc = self.tabViewControllers[1];
+    
+    [self show:mapsViewController andRemove:nvc];
+    
+}
+
+- (void)selectedCasesButton {
+    UIColor *selectedColor = [UIColor colorWithRed:35/255.0f green:41/255.0f blue:52/255.0f alpha:1.0f];
+    UIColor *unselectedColor = [UIColor colorWithRed:149/255.0f green:150/255.0f blue:151/255.0f alpha:1.0f];
+    
+    self.nearbyButton.imageView.image = [UIImage imageNamed:@"ic_nav_map_normal"];
+    self.nearbyButton.titleLabel.textColor = unselectedColor;
+    
+    self.casesButton.imageView.image = [UIImage imageNamed:@"ic_nav_cases_selected"];
+    self.casesButton.titleLabel.textColor = selectedColor;
+    
+    AggregateMapViewController *mapsViewController = self.tabViewControllers[0];
+    UINavigationController *nvc = self.tabViewControllers[1];
+    
+    [self show:nvc andRemove:mapsViewController];
 }
 
 - (void)show:(UIViewController *)newChild andRemove:(UIViewController *)oldChild {
@@ -69,44 +96,16 @@
     [oldChild.view removeFromSuperview];
     [oldChild removeFromParentViewController];
     
-    UIColor *backgroundColor;
-    UIColor *tabColor;
-    
-    UIColor* exploreBlue = [UIColor colorWithRed: 0.196f green: 0.325f blue: 0.682f alpha: 1];
-    UIColor* casesBlue = [UIColor colorWithRed: 0.106f green: 0.157f blue: 0.333f alpha: 1];
-    
-    if ([newChild isKindOfClass:[AggregateMapViewController class]]) {
-        backgroundColor = casesBlue;
-        tabColor = exploreBlue;
-    } else {
-        backgroundColor = exploreBlue;
-        tabColor = casesBlue;
-    }
+    UIColor *backgroundColor = [UIColor colorWithRed:231/255.0f green:232/255.0f blue:234/255.0f alpha:1.0f];
+    UIColor *dividerColor = [UIColor colorWithRed:205/255.0f green:205/255.0f blue:205/255.0f alpha:1.0f];
 
-    self.containerBackgroundView.backgroundColor = tabColor;
-    self.createReportView.backgroundColor = tabColor;
     self.view.backgroundColor = backgroundColor;
+    self.dividerView.backgroundColor = dividerColor;
     
     newChild.view.frame = self.containerView.bounds;
     [self.containerView addSubview:newChild.view];
     [self addChildViewController:newChild];
     [newChild didMoveToParentViewController:self];
-}
-
-- (IBAction)onTapTabBar:(UITapGestureRecognizer *)sender {
-    
-    CGPoint location = [sender locationInView:self.exploreTabBar];
-    if (location.x > 160) {
-        self.exploreTabBar.tabSelected = 1;
-        AggregateMapViewController *mapsViewController = self.tabViewControllers[0];
-        UINavigationController *nvc = self.tabViewControllers[1];
-        [self show:nvc andRemove:mapsViewController];
-    } else {
-        self.exploreTabBar.tabSelected = 0;
-        AggregateMapViewController *mapsViewController = self.tabViewControllers[0];
-        UINavigationController *nvc = self.tabViewControllers[1];
-        [self show:mapsViewController andRemove:nvc];
-    }
 }
 
 - (IBAction)onCreateReport:(id)sender {
@@ -117,5 +116,15 @@
     nvc.navigationBar.barTintColor = [UIColor colorWithRed: 0.196f green: 0.325f blue: 0.682f alpha: 1];
 
     [self presentViewController:nvc animated:YES completion:nil];
+}
+
+- (IBAction)onNearbyTab:(id)sender {
+    [self selectedNearbyButton];
+    
+}
+
+- (IBAction)onCasesTab:(id)sender {
+    [self selectedCasesButton];
+    
 }
 @end
