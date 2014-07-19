9ab76fb61bf8f593bb40935abdd402618962ed15
Nicolas Melo <nicolas@path.com>
2014-07-18 21:32:35 -0700

    Gonna try my hand at a tableview for a detail case.

---
+++ THC/CaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaseViewController.h
+//  THC
+//
+//  Created by Nicolas Melo on 7/18/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaseViewController : UIViewController
+
+@end
---
+++ THC/CaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  CaseViewController.m
+//  THC
+//
+//  Created by Nicolas Melo on 7/18/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import "CaseViewController.h"
+
+@interface CaseViewController ()
+
+@end
+
+@implementation CaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
