c93b6bab8fc9c48ce8c0dee973da648567de3678
Hunaid <hunaid@hotmail.com>
2014-07-21 13:55:18 -0700

    Report Creation new files

---
+++ THC/FieldContent.h
@@ -0,0 +1,13 @@
+//
+//  FieldContent.h
+//  THC
+//
+//  Created by Hunaid Hussain on 7/21/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FieldContent <NSObject>
+
+@end
