eebe2db0e46b3dd79df08e841e69524f426e05ee
Nicolas Melo <nicolas@path.com>
2014-07-20 23:32:16 -0700

    Login screen with new style. Needs work.

--- THC/LoginViewController.m
+++ THC/LoginViewController.m
@@ -30,6 +30,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
 }
 
 - (void)didReceiveMemoryWarning
