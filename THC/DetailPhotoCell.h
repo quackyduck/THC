7604bcd234cb642d6da67d99db8399f87306423c
Nicolas Melo <nicolas@path.com>
2014-07-22 12:25:28 -0700

    Adding the detail photo cell

---
+++ THC/DetailPhotoCell.h
@@ -0,0 +1,13 @@
+//
+//  DetailPhotoCell.h
+//  THC
+//
+//  Created by Nicolas Melo on 7/22/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailPhotoCell : UITableViewCell
+
+@end
---
+++ THC/DetailPhotoCell.m
@@ -0,0 +1,25 @@
+//
+//  DetailPhotoCell.m
+//  THC
+//
+//  Created by Nicolas Melo on 7/22/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import "DetailPhotoCell.h"
+
+@implementation DetailPhotoCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
