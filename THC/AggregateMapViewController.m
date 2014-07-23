e06fd7af0cccdee31003e1ac10302a60b1e9c9b2
Nicolas Melo <nicolas@path.com>
2014-07-22 18:56:02 -0700

    Image pins with number of open cases in them

--- THC/AggregateMapViewController.m
+++ THC/AggregateMapViewController.m
@@ -47,11 +47,11 @@
 -(void)zoomInToTenderloin
 {
     MKCoordinateRegion region = { {0.0, 0.0 }, { 0.0, 0.0 } };
-    region.center.latitude = 37.785134;
+    region.center.latitude = 37.78;
     region.center.longitude = -122.412752;
     
-    region.span.longitudeDelta = 0.03f;
-    region.span.latitudeDelta = 0.03f;
+    region.span.longitudeDelta = 0.05f;
+    region.span.latitudeDelta = 0.001f;
     [self.mapView setRegion:region animated:YES];
 }
 
@@ -60,12 +60,6 @@
     [super viewDidLoad];
     self.mapView.delegate = self;
     [self zoomInToTenderloin];
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    
-    [self.navigationController setNavigationBarHidden:YES animated:animated];
-    [super viewWillAppear:animated];
     
     PFUser *currentUser = [PFUser currentUser];
     if (currentUser) {
@@ -86,7 +80,12 @@
             NSLog(@"Error: %@ %@", error, [error userInfo]);
         }
     }];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
     
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
+    [super viewWillAppear:animated];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -120,12 +119,10 @@
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
     
     static NSString* AnnotationIdentifier = @"Annotation";
-    BuildingAnnotationView *annotationView = (BuildingAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:AnnotationIdentifier];
+    MKAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:AnnotationIdentifier];
     if (!annotationView) {
-        annotationView = [[BuildingAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:AnnotationIdentifier];
-        annotationView.frame = CGRectMake(0, 0, 35, 35);
-        annotationView.backgroundColor = [UIColor clearColor];
         
+        annotationView = [[MKAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:AnnotationIdentifier];
         Building *building = (Building *)annotation;
         
         PFQuery *query = [PFQuery queryWithClassName:@"Case"];
@@ -134,20 +131,35 @@
             if (!error) {
                 // The find succeeded.
                 NSLog(@"Successfully got %lu cases for building %@", (unsigned long)objects.count, building.buildingName);
-//                [self.buildingInfo setValue:@(objects.count) forKey:building.objectId];
-                annotationView.numberOfCases = objects.count;
+                NSString *text = [NSString stringWithFormat:@"%d", objects.count];
+                UIImage *pin = [UIImage imageNamed:@"btn_map_pin_normal"];
+                CGPoint point = CGPointMake(annotationView.bounds.origin.x + pin.size.width / 2.5f, annotationView.bounds.origin.y + pin.size.height / 3);
+                
+                UIFont *font = [UIFont systemFontOfSize:14];
+                UIGraphicsBeginImageContextWithOptions(pin.size, NO, 0);
+                [pin drawInRect:CGRectMake(0, 0, pin.size.width, pin.size.height)];
+                CGRect rect = CGRectMake(point.x, point.y, pin.size.width, pin.size.height);
+                [text drawInRect:rect withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName:[UIColor whiteColor]}];
+                UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+                UIGraphicsEndImageContext();
+                
+                
+                
+                annotationView.image = newImage;
                 [annotationView setNeedsDisplay];
+
                 
             } else {
                 // Log details of the failure
                 NSLog(@"Error: %@ %@", error, [error userInfo]);
             }
         }];
+//
+//        
+//        
+//        annotationView.numberOfCases = [[self.buildingInfo valueForKey:building.objectId] intValue];
         
         
-        
-        annotationView.numberOfCases = [[self.buildingInfo valueForKey:building.objectId] intValue];
-        [annotationView setNeedsDisplay];
         
     }
     else {
@@ -157,6 +169,35 @@
     annotationView.canShowCallout = YES;
     
     return annotationView;
+}
+
+-(UIImage *)addText:(UIImage *)img text:(NSString *)text1{
+    int w = img.size.width;
+    int h = img.size.height;
+    //lon = h - lon;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(NULL, w, h, 8, 4 * w, colorSpace, kCGImageAlphaPremultipliedFirst);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, w, h), img.CGImage);
+    CGContextSetRGBFillColor(context, 0.0, 0.0, 1.0, 1);
+	
+    char* text	= (char *)[text1 cStringUsingEncoding:NSASCIIStringEncoding];// "05/05/09";
+    CGContextSelectFont(context, "Arial", 18, kCGEncodingMacRoman);
+    CGContextSetTextDrawingMode(context, kCGTextFill);
+    CGContextSetRGBFillColor(context, 255, 255, 255, 1);
+	
+    
+    //rotate text
+    CGContextSetTextMatrix(context, CGAffineTransformMakeRotation( -M_PI/4 ));
+	
+    CGContextShowTextAtPoint(context, 4, 52, text, strlen(text));
+	
+	
+    CGImageRef imageMasked = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+	
+    return [UIImage imageWithCGImage:imageMasked];
 }
 
 @end
