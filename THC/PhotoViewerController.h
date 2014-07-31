5810d83c6ce87238ac224c19d92cbc9bb41c316b
Hunaid <hunaid@hotmail.com>
2014-07-31 08:42:35 -0700

    A PhotoViewer skeleton

---
+++ THC/PhotoViewerController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewerController.h
+//  THC
+//
+//  Created by Hunaid Hussain on 7/31/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewerController : UIViewController
+
+@end
---
+++ THC/PhotoViewerController.m
@@ -0,0 +1,38 @@
+//
+//  PhotoViewerController.m
+//  THC
+//
+//  Created by Hunaid Hussain on 7/31/14.
+//  Copyright (c) 2014 THC. All rights reserved.
+//
+
+#import "PhotoViewerController.h"
+
+@interface PhotoViewerController ()
+
+@end
+
+@implementation PhotoViewerController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
