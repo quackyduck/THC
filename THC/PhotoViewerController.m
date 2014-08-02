254b9c3e2043418441fb752d4d4b46b2ca414274
Hunaid <hunaid@hotmail.com>
2014-08-02 14:48:58 -0700

    minor changes in photo viewer

--- THC/PhotoViewerController.m
+++ THC/PhotoViewerController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIScrollView     *scrollView;
 @property (strong, nonatomic) NSArray                 *imagesToShow;
 @property (strong, nonatomic) NSArray                 *imageOrientations;
+@property (strong, nonatomic) NSMutableDictionary     *imageToImageView;
 
 
 
@@ -47,6 +48,7 @@
     self.scrollView.showsVerticalScrollIndicator = NO;
     self.scrollView.userInteractionEnabled = YES;
     
+    
 //    [self.scrollView setContentMode:UIViewContentModeScaleAspectFit];
     
     // Set them up in the scroll view
@@ -60,6 +62,8 @@
         UISwipeGestureRecognizer *downSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(dismiss:)];
         [downSwipe setDirection:UISwipeGestureRecognizerDirectionDown];
         
+        UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(createNoteBox:)];
+
 
         CGRect imageViewFrame = self.scrollView.frame;
 //        NSLog(@"imageview frame: %@", NSStringFromCGRect(imageViewFrame));
@@ -86,8 +90,11 @@
 //        imageView.clipsToBounds = YES;
 
 //        NSLog(@"imageView frame %@", NSStringFromCGRect(imageViewFrame));
+
         imageView.userInteractionEnabled = YES;
         [imageView addGestureRecognizer:downSwipe];
+        [imageView addGestureRecognizer:tapGesture];
+        NSLog(@"image size  %f , %f", imageView.image.size.width, imageView.image.size.height);
 
         [self.scrollView addSubview:imageView];
         ++imageCount;
@@ -116,5 +123,80 @@
     if (swipe.direction == UISwipeGestureRecognizerDirectionDown || swipe.direction == UISwipeGestureRecognizerDirectionUp) {
         [self dismissViewControllerAnimated:YES completion:nil];
     }
+}
+
+- (void)createNoteBox:(UITapGestureRecognizer *) tap {
+    
+    CGPoint centre = [tap locationInView:tap.view];
+    UIView  *view = tap.view;
+//    UIImageView *imageView = (UIImageView *) tap.view;
+    
+    
+    
+//    NSLog(@"View frame %@", NSStringFromCGRect(view.frame));
+
+    CGRect textFrame = CGRectMake(centre.x, centre.y, 120, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFrame];
+    [textField setFont:[UIFont boldSystemFontOfSize:25]];
+    [textField setTextColor:[UIColor whiteColor]];
+    [view addSubview:textField];
+    
+//    imageView.image = [self screenshot:imageView.image.size];
+//    imageView.image = [self Snapshot:imageView];
+}
+
+
+-(UIImage *)screenshot:(CGSize) imageSize
+{
+//    CGSize imageSize = CGSizeZero;
+//    
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+//    if (UIInterfaceOrientationIsPortrait(orientation)) {
+//        imageSize = [UIScreen mainScreen].bounds.size;
+//    } else {
+//        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+//    }
+    
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
+        CGContextSaveGState(context);
+        CGContextTranslateCTM(context, window.center.x, window.center.y);
+        CGContextConcatCTM(context, window.transform);
+        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+        if (orientation == UIInterfaceOrientationLandscapeLeft) {
+            CGContextRotateCTM(context, M_PI_2);
+            CGContextTranslateCTM(context, 0, -imageSize.width);
+        } else if (orientation == UIInterfaceOrientationLandscapeRight) {
+            CGContextRotateCTM(context, -M_PI_2);
+            CGContextTranslateCTM(context, -imageSize.height, 0);
+        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+            CGContextRotateCTM(context, M_PI);
+            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+        }
+        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)]) {
+            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+        } else {
+            [window.layer renderInContext:context];
+        }
+        CGContextRestoreGState(context);
+    }
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+-(UIImage *) Snapshot:(UIImageView *) imageView
+{
+    UIImage *image;
+    UIGraphicsBeginImageContext(imageView.frame.size);
+    //new iOS 7 method to snapshot
+    [imageView drawViewHierarchyInRect:imageView.frame afterScreenUpdates:YES];
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+    //...code to save to photo album omitted for brevity
 }
 @end
