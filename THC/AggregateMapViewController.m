363fe990fb7c5b058f69f19600ee310921dc5f30
Nicolas Melo <nicolas@path.com>
2014-08-02 23:46:24 -0700

    Moved configure annotation stuff to model. Added the open case number to the selected pin.

--- THC/AggregateMapViewController.m
+++ THC/AggregateMapViewController.m
@@ -130,66 +130,8 @@
         annotationView.canShowCallout = NO;
         
         Building *building = (Building *)annotation;
+        [annotationView configureAnnotationWithBuilding:building];
 
-        //Get first image to show
-        PFQuery *photoQuery = [BuildingPhoto query];
-        [photoQuery whereKey:@"buildingId" equalTo:building.objectId];
-        [photoQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-            if (!error) {
-                if (objects.count > 0)
-                {
-                    BuildingPhoto* photoObject = objects[0];
-                    PFFile *photo = photoObject.image;
-                    [photo getDataInBackgroundWithBlock:^(NSData *data, NSError *photoError) {
-                        if (!photoError) {
-                            NSData *imageData = data;
-                            UIImage *image = [UIImage imageWithData:imageData];
-                            
-                            CGRect resizeRect = CGRectMake(0, 0, 32, 32);
-                            UIGraphicsBeginImageContext(resizeRect.size);
-                            [image drawInRect:resizeRect];
-                            UIImage *resizedImage = UIGraphicsGetImageFromCurrentImageContext();
-                            UIGraphicsEndImageContext();
-                            annotationView.leftCalloutAccessoryView = [[UIImageView alloc] initWithImage:resizedImage];
-
-                        }
-                    }];
-                    
-                }
-            }
-        }];
-        
-        PFQuery *query = [PFQuery queryWithClassName:@"Case"];
-        [query whereKey:@"buildingId" equalTo:building.objectId];
-        [query whereKey:@"status" equalTo:@0];
-        [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-            if (!error) {
-                // The find succeeded.
-                NSLog(@"Successfully got %lu cases for building %@", (unsigned long)objects.count, building.buildingName);
-                NSString *text = [NSString stringWithFormat:@"%lu", objects.count];
-                UIImage *pin = [UIImage imageNamed:@"btn_map_pin_normal"];
-                CGPoint point = CGPointMake(annotationView.bounds.origin.x + pin.size.width / 2.5f, annotationView.bounds.origin.y + 15);
-                
-                UIFont *font = [UIFont boldSystemFontOfSize:16];
-                UIGraphicsBeginImageContextWithOptions(pin.size, NO, 0);
-                [pin drawInRect:CGRectMake(0, 0, pin.size.width, pin.size.height)];
-                CGRect rect = CGRectMake(point.x, point.y, pin.size.width, pin.size.height);
-                [text drawInRect:rect withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName:[UIColor whiteColor]}];
-                UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-                UIGraphicsEndImageContext();
-                
-                annotationView.image = newImage;
-                annotationView.center = CGPointMake(annotationView.center.x, annotationView.center.y - 20);
-                
-                [UIView animateWithDuration:0.4 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.7 options:UIViewAnimationOptionCurveEaseOut animations:^{
-                    annotationView.center = CGPointMake(annotationView.center.x, annotationView.center.y + 20);
-                } completion:nil];
-                [annotationView setNeedsDisplay];
-            } else {
-                // Log details of the failure
-                NSLog(@"Error: %@ %@", error, [error userInfo]);
-            }
-        }];
     }
     else {
         annotationView.annotation = annotation;
@@ -198,24 +140,40 @@
     return annotationView;
 }
 
-//- (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray *)annotationViews
-//{
-//    for (MKAnnotationView *annView in annotationViews)
-//    {
-//        NSLog(@"y! %f", annView.center.y);
-//        annView.center = CGPointMake(annView.center.x, annView.center.y - 20);
-//        annView.layer.opacity = 0;
-//        [UIView animateWithDuration:0.3 delay:0.5 options:UIViewAnimationOptionCurveEaseOut animations:^{
-//            annView.center = CGPointMake(annView.center.x, annView.center.y + 20);
-//            annView.alpha = 1;
-//        } completion:nil];
-//    }
-//}
-
 - (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
     
     BuildingMapPin *annotationView = (BuildingMapPin *) view;
-    annotationView.image = [UIImage imageNamed:@"btn_map_pin_selected"];
+    
+    Building *building = (Building *)view.annotation;
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"Case"];
+    [query whereKey:@"buildingId" equalTo:building.objectId];
+    [query whereKey:@"status" equalTo:@0];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            NSLog(@"Successfully got %lu cases for building %@", (unsigned long)objects.count, building.buildingName);
+            NSString *text = [NSString stringWithFormat:@"%lu", objects.count];
+            UIImage *pin = [UIImage imageNamed:@"btn_map_pin_selected"];
+            CGPoint point = CGPointMake(annotationView.bounds.origin.x + pin.size.width / 2.5f, annotationView.bounds.origin.y + 15);
+            
+            UIFont *font = [UIFont boldSystemFontOfSize:16];
+            UIGraphicsBeginImageContextWithOptions(pin.size, NO, 0);
+            [pin drawInRect:CGRectMake(0, 0, pin.size.width, pin.size.height)];
+            CGRect rect = CGRectMake(point.x, point.y, pin.size.width, pin.size.height);
+            [text drawInRect:rect withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName:[UIColor whiteColor]}];
+            UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            
+            annotationView.image = newImage;
+            annotationView.center = CGPointMake(annotationView.center.x, annotationView.center.y - 20);
+            
+            [annotationView setNeedsDisplay];
+        } else {
+            // Log details of the failure
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+    }];
     
     
     [mapView setRegion:[mapView convertRect:view.frame toRegionFromView:mapView] animated:YES];
@@ -235,9 +193,7 @@
     [buildingCallout.layer setBorderColor:[[UIColor whiteColor] CGColor]];
     [buildingCallout.layer setBorderWidth:0.5f];
     
-    
-    
-    Building *building = (Building *)view.annotation;
+//    Building *building = (Building *)view.annotation;
     
     buildingCallout.hotelNameLabel.text = building.buildingName;
     buildingCallout.hotelDescriptionLabel.text = building.streetAddress;
@@ -265,10 +221,10 @@
                         buildingCallout.violationsCountLabel.textAlignment = NSTextAlignmentCenter;
                         buildingCallout.violationsCountLabel.backgroundColor = [UIColor colorWithRed: 1 green: 0.455f blue: 0.184f alpha: 1];
                         
-                        PFQuery *query = [PFQuery queryWithClassName:@"Case"];
-                        [query whereKey:@"buildingId" equalTo:building.objectId];
-                        [query whereKey:@"status" equalTo:@0];
-                        [query findObjectsInBackgroundWithBlock:^(NSArray *caseObjects, NSError *queryError) {
+                        PFQuery *caseQuery = [Case query];
+                        [caseQuery whereKey:@"buildingId" equalTo:building.objectId];
+                        [caseQuery whereKey:@"status" equalTo:@0];
+                        [caseQuery findObjectsInBackgroundWithBlock:^(NSArray *caseObjects, NSError *queryError) {
                             if (!error) {
                                 // The find succeeded.
                                 NSLog(@"Successfully got %lu cases for building %@", (unsigned long)objects.count, building.buildingName);
